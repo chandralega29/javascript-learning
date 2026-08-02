b363f5851fb0ba17be395754e6cb3cb58132dfd0
chandra <chandralega103@gmail.com>
2026-08-02 17:28:21 +0530

    Add Control Flow Statements notes and examples

---
+++ else_if.js
@@ -0,0 +1,18 @@
+// JavaScript if...else if...else Statement
+// The if...else if...else statement is used when you want to handle multiple conditions.
+
+// if_else_if_else_statement
+
+
+
+const temp = 25;
+if (temp > 30) {
+    console.log("It's hot.");
+} else if (temp >= 20) {
+    console.log("It's warm.");
+} else {
+    console.log("It's cold.");
+}
+// Checks if the temperature is greater than 30, logs "It's hot."
+// If not, checks if it's between 20 and 30, logs "It's warm."
+// Otherwise, logs "It's cold."
